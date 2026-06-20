a9168a495148e879f9e91d447a33bd729ae438d1
Raajitha Samudrala <raajithasamudrala6@gmail.com>
2026-06-20 10:41:34 +0530

    binary search

---
+++ first_occurence.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] arr={1,1,1,1,2,2,2,3,3,3,3,3,4,5,5};
+        int n=arr.length;
+        int k=2;
+        int l=0,r=n-1;
+        int idx=-1;
+        boolean f=false;
+        while(l<=r){
+            int m=(l+r)/2;
+            if(arr[m]==k){
+                f=true;
+                idx=m;
+                r=m-1;
+                
+            }
+            else if(arr[m]>k) r=m-1;
+            else l=m+1;
+        }
+        System.out.println(idx);
+    }
+}
